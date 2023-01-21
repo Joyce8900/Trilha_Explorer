953706f553586ca06e61b86a5b8a5f99cf96d187
Joyce <joycesantos8900@gmail.com>
2023-01-21 17:51:04 -0300

    Aula 4

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button')
 
-const data = {
-    run: ["01-01", "01-02", "01-06"],
-    water: ["01-04"],
-    food: ["01-05"],
-    libre : ["01-07"],
-    cheers: ["01-08"]
+button.addEventListener('click', add)
+form.addEventListener('change', save)
 
-};
+function add (){
+    const today = new Date().toLocaleDateString('pt-br').slice(0,-5);
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert('Dia já incluso!❌')
+        return 
+    }
+    
+    alert ('Adicionado com sucesso ✅')
+    nlwSetup.addDay(today)
+
+}
+function save (){
+    console.log(nlwSetup.data)
+    window.localStorage.setItem('NLWSetup@habits',JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
